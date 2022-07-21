6cb16913f9c4af173bd7bf0c0c2485cfd31a553e
Your Name <you@example.com>
2022-07-22 00:42:14 +0200

    add navlinke

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,29 +1,34 @@
 import React, { Component } from "react";
+import { Link, NavLink } from "react-router-dom";
 class Nav extends Component {
   state = {};
   render() {
     return (
       <>
-        <ul class="nav justify-content-center">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">
-              Home
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              Tools
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              About us
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link ">Call us</a>
-          </li>
-        </ul>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3 p-2">
+          <Link className="navbar-brand" to="/">
+            Admin
+          </Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav">
+              <NavLink className=" nav-item nav-link" to="">
+                Movies
+              </NavLink>
+              <NavLink className="nav-item nav-link" to="">
+                Customer
+              </NavLink>
+              <NavLink className="nav-item nav-link" to="">
+                Rentals
+              </NavLink>
+              <NavLink className="nav-item nav-link" to="">
+                Login
+              </NavLink>
+              <NavLink className="nav-item nav-link" to="">
+                Register
+              </NavLink>
+            </ul>
+          </div>
+        </nav>
       </>
     );
   }
